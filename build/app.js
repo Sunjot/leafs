de349d4756b84445ab719170ff4f8db125525b4f
Sunjot Singh <sunjot@uoguelph.ca>
2020-02-26 17:47:19 -0500

    compiled updated ts files

--- build/app.js
+++ build/app.js
@@ -1,10 +1,9 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -47,13 +46,14 @@ function getYears(req, res, next) {
         next();
     });
 }
-app.post('/api/players', getYears, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/players', getYears, (req, res) => __awaiter(this, void 0, void 0, function* () {
     // When calling the route as a result of an action (like changing the year itself on the page), 
     // the year will get passed as part of the body and so that will be used instead
     let yearShort = req.body.year ? req.body.year.replace("-", "") : res.locals.years[res.locals.years.length - 1].replace("-", "");
     let URLs = [];
     req.body.cats.map((cat) => {
-        let URL = "https://api.nhle.com/stats/rest/skaters?isAggregate=false&reportType=" + req.body.type + "&isGame=false&reportName=skater" + req.body.report + "&sort=[{%22property%22:%22" + cat + "%22,%22direction%22:%22DESC%22}]&factCayenneExp=gamesPlayed%3E=40&cayenneExp=leagueId=133%20and%20gameTypeId=2%20and%20seasonId%3E=" + yearShort + "%20and%20seasonId%3C=" + yearShort + "%20and%20teamId=10";
+        /* let URL = "https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[{%22property%22:%22" + cat + "%22,%22direction%22:%22DESC%22}]&factCayenneExp=gamesPlayed%3E=40&cayenneExp=leagueId=133%20and%20gameTypeId=2%20and%20seasonId%3E=" + yearShort + "%20and%20seasonId%3C=" + yearShort + "%20and%20teamId=10"; */
+        let URL = "https://api.nhle.com/stats/rest/en/skater/" + req.body.report + "?isAggregate=false&isGame=false&sort=[{%22property%22:%22" + cat + "%22,%22direction%22:%22DESC%22}]&start=0&limit=50&factCayenneExp=gamesPlayed%3E=1&cayenneExp=franchiseId%3D5%20and%20gameTypeId=2%20and%20seasonId%3C=" + yearShort + "%20and%20seasonId%3E=" + yearShort;
         URLs.push(URL);
     });
     let [goals, assists, points] = yield Promise.all([
